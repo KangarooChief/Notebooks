4e06122332028520f15dcf6bd92e2ed3c2e2eda7
Charlie <charliehennys@gmail.com>
2020-05-26 23:54:56 +0100

    Added alice

---
+++ 2020 p6 q7.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy as sp
+from scipy import stats
+import matplotlib.pyplot as plt
+
+p=0.5
+q=0.5
+n=100
+
+t=[[1-p,p],[q,1-q]]
+sd = 0.5
+
+def rx(n):
+  Xs = [np.random.choice([0,1],p=[q/(p+q),p/(p+q)])]
+  for i in range(n):
+
+    Xs.append(np.random.choice([0,1],p=[t[Xs[-1]][0],t[Xs[-1]][1]]))
+  return Xs
+
+actualXs = rx(n)
+actualYs = np.random.normal(loc=actualXs, scale=sd)
+
+def genWeights(m):
+    Xs_samples = [rx(n) for i in range(m)]
+    PrY = [np.prod(stats.norm.pdf(actualYs, loc=Xs, scale=sd)) for Xs in Xs_samples]
+    ws = PrY/sum(PrY)
+    print(len(ws))
+    plt.hist([i for i in range(m)],weights=ws, bins=np.linspace (0,1,30))
+    plt.show()
+    return ws
+
+ws = genWeights(101)
---
+++ alice.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+f=open("alice.txt", "r")
+contents = f.read()
+
+previous = " "
+bigram  = {}
+totals = {}
+for i in contents:
+    if (previous+i) not in bigram.keys():
+        bigram[previous+i] = 0
+    bigram[previous+i] += 1
+    if i not in totals.keys():
+        totals[i] = 0
+    totals[i] += 1
+    previous = i
+
+#Normalise
+for (k,v) in bigram.items():
+    bigram[k] = bigram[k]/totals[k[0]]
+
+def gen():
+    #Generate
+    word = " "
+    while True:
+        choices = {k:v for k,v in list(bigram.items()) if k[0] == word[-1]}
+        word += np.random.choice(list(choices.keys()), p=list(choices.values()))[1]
+        if word[-1] == " ":
+            break
+    return word[1:]
+
+book = ""
+
+for i in range(100):
+    book += gen() + " "
+
+print(book)
+
